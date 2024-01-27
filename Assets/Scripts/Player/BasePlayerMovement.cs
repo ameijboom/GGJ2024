0a14cd01fc95ba02bfb605fd7e4796d579c9db9a
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-01-27 12:19:38 +0100

    Played around with wonky player movement

---
+++ Assets/Scripts/Player/BasePlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Player
+{
+	public class BasePlayerMovement : MonoBehaviour
+	{
+		[SerializeField] private float movementSpeed;
+		[SerializeField] private float rotationSpeed;
+
+		private Rigidbody _rb;
+
+		private void Awake()
+		{
+			_rb = GetComponent<Rigidbody>();
+		}
+
+		private Quaternion _direction = Quaternion.identity;
+
+		private void FixedUpdate()
+		{
+			float horizontal = Input.GetAxis("Horizontal");
+			//rotate direction according to horizontal input
+			_direction *= Quaternion.AngleAxis(horizontal * rotationSpeed * Time.fixedDeltaTime, Vector3.up);
+			_rb.rotation = _direction;
+			//move player in the direction it's facing
+			_rb.AddForce(_direction * Vector3.forward * (movementSpeed * Time.fixedDeltaTime), ForceMode.Force);
+		}
+	}
+}
