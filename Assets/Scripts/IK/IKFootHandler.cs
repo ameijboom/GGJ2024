17a414af5a20925b92ae16eb759387711cb9474f
Alwin <alwin@brainhive.nl>
2024-01-27 22:28:59 +0100

    add github actions for unity webgl builds

--- Assets/Scripts/IK/IKFootHandler.cs
+++ Assets/Scripts/IK/IKFootHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 
