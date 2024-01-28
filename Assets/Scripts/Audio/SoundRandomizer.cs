7e21044283b495e6df5d5064d28c53ccad2ee24a
Adrian Georgiev <andigebg@gmail.com>
2024-01-28 03:35:37 +0100

    Add audio source and scripts

---
+++ Assets/Scripts/Audio/SoundRandomizer.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class SoundRandomizer : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        
+    }
+}
